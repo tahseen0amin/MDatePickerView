a1e670292d7a13d2d266bffbe1a1ae7c58c616db
Tasin Zarkoob <tasin.amin@mcdermott.com>
2020-04-30 23:26:07 +0530

    Package Created

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MDatePickerView",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,8 +22,5 @@ let package = Package(
         .target(
             name: "MDatePickerView",
             dependencies: []),
-        .testTarget(
-            name: "MDatePickerViewTests",
-            dependencies: ["MDatePickerView"]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import MDatePickerViewTests
-
-var tests = [XCTestCaseEntry]()
-tests += MDatePickerViewTests.allTests()
-XCTMain(tests)
